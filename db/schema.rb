b4dfec9198385961816f4e915732a7859788cc67
Mary Traynor <maryotraynor@gmail.com>
2023-03-03 20:13:10 +0000

    g model Rec

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_175228) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_200726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recs", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_recs_on_category_id"
+    t.index ["user_id"], name: "index_recs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_175228) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recs", "categories"
+  add_foreign_key "recs", "users"
 end
