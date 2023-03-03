630e89668f2f4b6d486851e8944b12692e25d523
Mary Traynor <maryotraynor@gmail.com>
2023-03-03 20:16:17 +0000

    g model Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_200726) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_201538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_200726) do
     t.index ["user_id"], name: "index_recs_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.bigint "user_id", null: false
+    t.bigint "rec_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rec_id"], name: "index_reviews_on_rec_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_200726) do
 
   add_foreign_key "recs", "categories"
   add_foreign_key "recs", "users"
+  add_foreign_key "reviews", "recs"
+  add_foreign_key "reviews", "users"
 end
