f7fdeda207f3f60cd912a4697b7531de653c6e35
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-03-05 22:23:57 +0000

    added methods to controller and started pundit

--- app/controllers/recs_controller.rb
+++ app/controllers/recs_controller.rb
@@ -1,2 +1,58 @@
 class RecsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_rec, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @recs = Rec.all
+    # @recs = policy_scope(rec)
+  end
+
+  def show
+    # @review = Review.new
+    # authorize @rec
+  end
+
+  def new
+    @rec = Rec.new
+    # authorize @rec
+  end
+
+  def create
+    @rec = Rec.new(rec_params)
+    @rec.user = current_user
+    # authorize @rec
+    if @rec.save
+      redirect_to rec_path(@rec)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # authorize @rec
+    @rec.destroy
+    redirect_to recs_path
+  end
+
+  def edit
+    # authorize @rec
+  end
+
+  def update
+    # authorize @rec
+    @rec.update(rec_params)
+    redirect_to rec_path(@rec)
+    # else
+    # render "edit" #will go back / stay on the same page
+  end
+
+  private
+
+  def rec_params
+    params.require(:rec).permit(:name, :address, :description, photos: [])
+  end
+
+  def set_rec
+    @rec = Rec.find(params[:id])
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @rec = Rec.find(params[:item_id])
+    # authorize @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.rec = Rec.find(params[:item_id])
+    # authorize @review
+    if @review.save!
+      redirect_to rec_path(@rec), notice: 'review request successful'
+    else
+      redirect_to new_rec_review_path(@rec), status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    # authorize @review
+    @review.destroy
+    redirect_to rec_path(@rec), status: :see_other
+  end
+
+  private
+
+  def review
+    params.require(:review).permit(:rating, :comment)
+  end
+
 end
