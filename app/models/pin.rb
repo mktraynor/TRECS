68a267c0792b3648c22546a600122450c5d2eb97
Mary Traynor <maryotraynor@gmail.com>
2023-03-10 17:41:29 +0000

    changes w david

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,4 @@
 class Pin < ApplicationRecord
-  belongs_to :user
-  belongs_to :rec
-  belongs_to :board, through: :board_pin
+  belongs_to :user, :rec
+  belongs_to :boards, through: :board_pin
 end
