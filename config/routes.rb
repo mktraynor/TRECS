d71fc5af2c8e49186da4263a35129a6eb4d34112
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-03-25 16:36:01 +0000

    added reviews and username

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :recs do
-    resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+    resources :reviews
     resources :pins, only: [:new, :create]
   end
 
